b557a61fb72f59ed210d6375d332418ae8326662
dnxcoder <dnxcoder@gmail.com>
2025-06-09 14:23:03 -0500

    Enhance Pokédex UI

--- pokedexSwiftUI/Models/Pokemon.swift
+++ pokedexSwiftUI/Models/Pokemon.swift
@@ -8,9 +8,13 @@ struct PokemonEntry: Decodable {
 struct Pokemon: Identifiable {
     let id: Int
     let name: String
+    let types: [String]
+
     var imageURL: URL {
         URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png")!
     }
+
+    var primaryType: String { types.first ?? "" }
 }
 
 struct PokemonList: Decodable {

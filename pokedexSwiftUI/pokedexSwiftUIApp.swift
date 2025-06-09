260f87462475c6ecb3154a1346d182b747398fe2
Denis Coder <dnxcoder@gmail.com>
2025-06-09 14:09:27 -0500

    first commit

---
+++ pokedexSwiftUI/pokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexSwiftUIApp.swift
+//  pokedexSwiftUI
+//
+//  Created by Denis Coder on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct pokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pokedexSwiftUITests/pokedexSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexSwiftUITests.swift
+//  pokedexSwiftUITests
+//
+//  Created by Denis Coder on 6/9/25.
+//
+
+import Testing
+@testable import pokedexSwiftUI
+
+struct pokedexSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
